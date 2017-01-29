0454c4309feec933f8d071c9b6da1a87a6afbe88
Alex Sanchez <alex@s1ck.org>
2017-01-29 13:06:09 +0200

    minor optimisation

--- api/v1/methods.py
+++ api/v1/methods.py
@@ -5,6 +5,8 @@ from api.v1 import v1
 
 
 def calculate_fib(n):
+    if n == 0:
+        return [0]
     result = [0, 1]
     for i in range(2, n + 1):
         result.append(result[-1] + result[-2])
@@ -19,8 +21,10 @@ def fib(number):
     except ValueError:
         return jsonify({'error': 'Not an integer.'})
 
-    if number > 1000:
-        return jsonify({'error': 'Try a number less than 1000.'})
+    if number > 1000 or number < 0:
+        return jsonify(
+            {'error': 'The number should be in range from 0 to 1000.'})
+
     result = cache.get(number)
 
     if result is None:
